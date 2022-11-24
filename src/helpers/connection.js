061608c84a40919e1c62075eced4e5329c248bd2
Randy Sykes <rjsykes28@outlook.com>
2022-11-24 11:36:00 -0600

    fix(mongodb): Changed the connection so it would work with Mongo Atlas

--- src/helpers/connection.js
+++ src/helpers/connection.js
@@ -1,17 +1,23 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const { DB, URI, MONGO_PASSWORD, MONGO_USERNAME } = process.env;
-const endpoint = `mongodb://${URI}/${DB}`;
+const { MONGO_DB, MONGO_URI, MONGO_PASSWORD, MONGO_USERNAME } = process.env;
+// const endpoint = `mongodb://${MONGO_URL}/${MONGO_DB}`;
+const endpoint = MONGO_URI;
+
+// const connectionObject = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   authSource: "admin",
+//   user: MONGO_USERNAME,
+//   pass: MONGO_PASSWORD,
+// };
 
 const connectionObject = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  authSource: "admin",
-  user: MONGO_USERNAME,
-  pass: MONGO_PASSWORD,
 };
 
 mongoose
   .connect(endpoint, connectionObject)
-  .then(() => console.log(`Connected to ${DB} database`))
-  .catch((error) => console.log(`ERROR: connecting to ${DB}: `, error));
+  .then(() => console.log(`Connected to ${MONGO_DB} database`))
+  .catch((error) => console.log(`ERROR: connecting to ${MONGO_DB}: `, error));
