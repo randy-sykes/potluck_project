0f50670d7a1e5a0bd0425d55ae55cc319e6780bf
Malykai <rjsykes28@outlook.com>
2022-12-03 15:22:15 -0600

    update: Removed unneeded files from the backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV !== "production") {
   const morgan = require("morgan");
