79e047a4303b2f4e3d0749978cbcf454975c5792
Malykai <rjsykes28@outlook.com>
2022-12-06 17:25:00 -0600

    update: The README and helper to work for different 'environments'

--- server/helpers/connection.js
+++ server/helpers/connection.js
@@ -1,9 +1,10 @@
-require("dotenv").config();
+// Added this one so I could change the env files with .env.<ENV> i.e. .env.test and .env.dev
+require("custom-env").env();
+
 const mongoose = require("mongoose");
 const { MONGO_DB, MONGO_URL, MONGO_PASSWORD, MONGO_USERNAME } = process.env;
 // const endpoint = `mongodb://${MONGO_URL}/${MONGO_DB}`;
 const endpoint = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_URL}/${MONGO_DB}?retryWrites=true&w=majority`;
-
 
 const connectionObject = {
   useNewUrlParser: true,
