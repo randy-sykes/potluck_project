8ac17b80d57fcff74cd2a40172aa228611a40a39
Malykai <rjsykes28@outlook.com>
2022-12-07 15:52:14 -0600

    feature: Added check for existing email and user creation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,23 +1,37 @@
+const dataController = require("./dataController");
 // Will handle the actual work for the login check
 const loginUser = (req, res) => {
   res.send("POST login info to login to page.");
 };
 
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
   // Required keys for user obj
   const keys = ["first_name", "last_name", "email", "password"];
   const user = req.body;
   const missingFields = keys.filter((key) => !user.hasOwnProperty(key));
   if (Object.keys(user).length === 0) {
     res.status(400).json({ error: "Please provide a user object." });
-  } else if (missingFields) {
+    return;
+  } else if (missingFields.length !== 0) {
     res.status(400).json({
       error: "MissingFields",
       missingFields: missingFields,
     });
-  } else {
-    res.status(999).send("boo");
+    return;
   }
+  const userExists = await dataController.userExistsInDB(user);
+  if (userExists?._id.toString()) {
+    res.status(409).json({
+      error: "UserExists",
+      message: `User already exists with email: ${user.email}`,
+    });
+    return;
+  }
+  const createdUser = await dataController.createUserInDB(user);
+  res.status(201).json({
+    message: `Created user for email: ${createdUser.email}`,
+    userId: `${createdUser._id.toString()}`,
+  });
 };
 
 const getUser = (req, res) => {
