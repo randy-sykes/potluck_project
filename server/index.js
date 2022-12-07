6d8cbbf3048abf25908d32ccb23c5dbd18eda3aa
Malykai <rjsykes28@outlook.com>
2022-12-07 08:45:57 -0600

    test: split starting server from the server itself for testing

--- server/index.js
+++ server/index.js
@@ -1,40 +1,5 @@
-const express = require("express");
-const app = express();
-const swaggerUI = require("swagger-ui-express");
-const YAML = require("yamljs");
-const swaggerSpec = YAML.load("./docs/swagger.yaml");
-
+const app = require("./server");
 
 const PORT = process.env.PORT || 3001;
-
-// Load config from .env files
-require("dotenv-flow").config();
-
-if (process.env.NODE_ENV !== "production") {
-  const morgan = require("morgan");
-  app.use(morgan("dev"));
-}
-
-// To initialize the database
-require("./helpers/connection");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Load swagger docs:
-app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
-
-// Load recipes routes to /recipes
-app.use("/api/recipes", require("./routes/recipes"));
-app.use("/api/login", require("./routes/login"));
-
-// Load account routes to /account
-app.use("/api/account", require("./routes/account"));
-
-// Load base routes to /
-//  IMPORTANT:
-// ./routes/base has a wildcard in it to catch everything.
-// IT MUST BE AT THE BOTTOM!!!
-app.use(require("./routes/base"));
 
 app.listen(PORT, () => console.log(`potluck using port ${PORT}`));
