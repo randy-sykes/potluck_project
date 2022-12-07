453c7aaf0eaf046c913e6dfe04619f7e486b2df2
Malykai <rjsykes28@outlook.com>
2022-12-07 15:53:19 -0600

    fix: Corrected the setup/teardown functions so they are working properly

--- server/test/setup.test.js
+++ server/test/setup.test.js
@@ -2,29 +2,44 @@ process.env.NODE_ENV = "test";
 
 const { RecipeModel } = require("../models/recipe");
 const { UserModel } = require("../models/user");
-const mongoose = require("mongoose");
-const { MONGO_DB, MONGO_URL, MONGO_PASSWORD, MONGO_USERNAME } = process.env;
-// const endpoint = `mongodb://${MONGO_URL}/${MONGO_DB}`;
-const endpoint = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_URL}/${MONGO_DB}?retryWrites=true&w=majority`;
+require("../helpers/connection");
 
-// tells mongoose to use ES6 implementation of promises
-mongoose.Promise = global.Promise;
-mongoose.connect(endpoint);
-
-mongoose.connection
-  .once("open", () => {})
-  .on("error", (error) => {
-    console.warn("Error : ", error);
-  });
-
+// Cleanup Recipes collection before starting tests
 before((done) => {
-  RecipeModel.deleteMany({}, function (err) {});
-  UserModel.deleteMany({}, function (err) {});
-  done();
+  RecipeModel.deleteMany({})
+    .then(() => {
+      console.log("Before cleaned up after Recipes");
+      done();
+    })
+    .catch((err) => console.log("Before failed to delete Recipes:", err));
 });
 
+// Cleanup Users collection before starting tests
+before((done) => {
+  UserModel.deleteMany({})
+    .then(() => {
+      console.log("Before cleaned up after Users");
+      done();
+    })
+    .catch((err) => console.log("Before failed to delete Users:", err));
+});
+
+// Cleanup Recipes collection after completing tests
 after((done) => {
-  RecipeModel.deleteMany({}, function (err) {});
-  UserModel.deleteMany({}, function (err) {});
-  done();
+  RecipeModel.deleteMany({})
+    .then(() => {
+      console.log("After cleaned up after Recipes");
+      done();
+    })
+    .catch((err) => console.log("After failed to delete Recipes:", err));
+});
+
+// Cleanup Users collection after completing tests
+after((done) => {
+  UserModel.deleteMany({})
+    .then(() => {
+      console.log("After cleaned up after Users");
+      done();
+    })
+    .catch((err) => console.log("After failed to delete Users:", err));
 });
