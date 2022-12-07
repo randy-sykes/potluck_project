182162eaba6a420f23d55fefd9a9d2d10b91daf3
Malykai <rjsykes28@outlook.com>
2022-12-07 10:28:32 -0600

    fix: added check for console output to not happen for test/production envs

--- server/helpers/connection.js
+++ server/helpers/connection.js
@@ -10,5 +10,11 @@ const connectionObject = {
 
 mongoose
   .connect(endpoint, connectionObject)
-  .then(() => console.log(`Connected to ${MONGO_DB} database`))
+  .then(() => {
+    if (
+      process.env.NODE_ENV !== "test" &&
+      process.env.NODE_ENV !== "production"
+    )
+      console.log(`Connected to ${MONGO_DB} database`);
+  })
   .catch((error) => console.log(`ERROR: connecting to ${MONGO_DB}: `, error));
