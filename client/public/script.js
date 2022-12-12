137bf73487e5c1931ba07bca5254bc756450d19f
Malykai <rjsykes28@outlook.com>
2022-12-12 10:36:22 -0600

    update: added id to the body for create-recipe to load script.js only for that page

--- client/public/script.js
+++ client/public/script.js
@@ -1,20 +1,22 @@
-// Select the table that we are adding to
-const table = document.querySelector("table");
-// Select the first ingredient table row with inputs
-const ingredientRow = document.querySelectorAll("tr")[1];
-// Select the button to add ingredients
-const ingredientsBtn = document.querySelector("#addIngredientsBtn");
+if (document.getElementById("create-recipe")) {
+  // Select the table that we are adding to
+  const table = document.querySelector("table");
+  // Select the first ingredient table row with inputs
+  const ingredientRow = document.querySelectorAll("tr")[1];
+  // Select the button to add ingredients
+  const ingredientsBtn = document.querySelector("#addIngredientsBtn");
 
-ingredientsBtn.addEventListener("click", () => {
-  let newIngredient = ingredientRow.cloneNode(true);
-  let inputs = newIngredient.querySelectorAll("input");
-  inputs.forEach((input) => (input.value = ""));
-  table.appendChild(newIngredient);
-});
+  ingredientsBtn.addEventListener("click", () => {
+    let newIngredient = ingredientRow.cloneNode(true);
+    let inputs = newIngredient.querySelectorAll("input");
+    inputs.forEach((input) => (input.value = ""));
+    table.appendChild(newIngredient);
+  });
 
-function deleteRow(r) {
-  let element = r.parentNode.parentNode.rowIndex;
-  if (element > 1) {
-    document.getElementById("ingredientsList").deleteRow(element);
+  function deleteRow(r) {
+    let element = r.parentNode.parentNode.rowIndex;
+    if (element > 1) {
+      document.getElementById("ingredientsList").deleteRow(element);
+    }
   }
 }
