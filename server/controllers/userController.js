ece4e7c1bf2fbab2f1c2aa97b654cced75864ee0
Malykai <rjsykes28@outlook.com>
2022-12-16 08:18:39 -0600

    update: added userController for last commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -76,22 +76,22 @@ const createUser = async (req, res) => {
   });
 };
 
-const getUser = (req, res) => {
-  res.send("GET specific user account info.");
-};
+// const getUser = (req, res) => {
+//   res.send("GET specific user account info.");
+// };
 
-const updateUser = (req, res) => {
-  res.send("UPDATE specific user account info.");
-};
+// const updateUser = (req, res) => {
+//   res.send("UPDATE specific user account info.");
+// };
 
-const deleteUser = (req, res) => {
-  res.send("DELETE specific user account info.");
-};
+// const deleteUser = (req, res) => {
+//   res.send("DELETE specific user account info.");
+// };
 
 module.exports = {
   createUser,
-  getUser,
-  updateUser,
-  deleteUser,
+  // getUser,
+  // updateUser,
+  // deleteUser,
   loginUser,
 };
