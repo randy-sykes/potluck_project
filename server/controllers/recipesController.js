67cee819d1c99afb5a3a1efc6c310ea65f66f280
Malykai <rjsykes28@outlook.com>
2022-12-16 14:00:45 -0600

    feature: Setup the front end to be able to update a recipe

--- server/controllers/recipesController.js
+++ server/controllers/recipesController.js
@@ -1,6 +1,7 @@
 const dataController = require("./dataController");
 const { jwtValidation } = require("../helpers/validations");
 
+// Helper to validate recipe objects
 const validRecipeObj = (recipe) => {
   const recipeKeys = [
     "recipe_name",
@@ -32,11 +33,13 @@ const getAllRecipes = async (req, res) => {
 };
 
 const createNewRecipe = async (req, res) => {
+  // confirm there is a recipe
   if (req.body.recipe === undefined) {
     res.status(400).json({ error: "Please provide a recipe object." });
     return;
   }
   const recipe = req.body.recipe;
+  // validate recipe
   const missingFields = validRecipeObj(recipe);
   if (missingFields.length > 0) {
     res
@@ -44,6 +47,7 @@ const createNewRecipe = async (req, res) => {
       .json({ error: "MissingFields", missingFields: missingFields });
     return;
   }
+  // validate user
   const userTokenInfo = jwtValidation(req.header("auth-token"));
   const validUser = await dataController.userExistsInDB(
     "_id",
@@ -56,6 +60,7 @@ const createNewRecipe = async (req, res) => {
     });
     return;
   }
+  // check if recipe exists in the database
   const recipeExists = await dataController.doesRecipeExistInDB(
     recipe.recipe_name
   );
@@ -63,6 +68,7 @@ const createNewRecipe = async (req, res) => {
     res.status(409).json({ error: "Recipe already exists with that name." });
     return;
   }
+  // add user id as author
   recipe.author = userTokenInfo._id;
   const newRecipe = await dataController.createNewRecipeInDB(recipe);
   if (newRecipe?.error) {
@@ -73,17 +79,21 @@ const createNewRecipe = async (req, res) => {
 };
 
 const getSpecificRecipe = async (req, res) => {
+  // get recipe id from params
   const recipe_id = req.params.recipe_id;
+  // get it from the database
   const recipe = await dataController.getRecipeInDB(recipe_id);
   if (recipe?.error || recipe === null) {
     return res.status(404).json(recipe);
   }
+  // add author name to the recipe and return it
   const author = await dataController.getUserFromDB("_id", recipe.author);
   recipe.author_name = author.full_name;
   res.status(200).json(recipe);
 };
 
 const updateSpecificRecipe = async (req, res) => {
+  // ensure there is a recipe object with valid fields
   if (req.body.recipe === undefined) {
     res.status(400).json({ error: "Please provide a recipe object." });
     return;
@@ -96,6 +106,7 @@ const updateSpecificRecipe = async (req, res) => {
       .json({ error: "MissingFields", missingFields: missingFields });
     return;
   }
+  // verify user
   const userTokenInfo = jwtValidation(req.header("auth-token"));
   const validUser = await dataController.userExistsInDB(
     "_id",
@@ -108,13 +119,15 @@ const updateSpecificRecipe = async (req, res) => {
     });
     return;
   }
+
+  // ensure author owns the recipe
   if (recipe.author !== userTokenInfo._id) {
     return res.status(401).json({
       error: "InvalidAuthor",
       message: "User does not own recipe.",
     });
   }
-
+  // update the recipe and return result/error
   const updateRecipe = await dataController.updateSpecificRecipeInDB(recipe);
   if (updateRecipe?.error === "UpdateFailed") {
     return res.status(400).json(updateRecipe);
